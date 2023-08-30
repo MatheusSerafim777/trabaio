ed125c960ba1a79cb75a2d069ceddbf2ea8285b8
Xumiro <98985939+Xumiro@users.noreply.github.com>
2023-08-30 14:36:29 -0300

    Update trabaio.c

--- trabaio.c
+++ trabaio.c
@@ -10,16 +10,48 @@ void definirconjunto(int(*quantA),int(*quantB)){
     scanf("%d",quantB);
 }
 
+void inserirConj(int conj[],int tamanho){
+    int i,c;
+    for (i=0;i<tamanho;i++){
+        do{
+        printf("\nEscreva o numero: ");
+        scanf("%d",&conj[i]);
+        for (c=0;c<tamanho;c++){
+            if(conj[i]=conj[c]);
+        }
+        
+        }
+    }
+}
 
-void main()
+void imprimir(int conj[], int tamanho){
+    int i;
+        for (i=0;i<tamanho;i++){
+            printf(" %d",conj[i]);
+     
+        }
+}
+
+
+int main()
 {
-    int quantA,quantB,tamanho;
+    int quantA,quantB;
     definirconjunto(&quantA,&quantB);
     int *conjA = malloc(quantA * sizeof(int));
     int *conjB = malloc(quantB * sizeof(int));
-    tamanho = sizeof(conjA)/sizeof(int);
-    printf("A quantidade e: %d, %d, %d", quantA,quantB,tamanho);
-}
+    printf("\nCONJUNTO A----------------------");
+    inserirConj(conjA,quantA);
+    printf("\nCONJUNTO B----------------------");
+    inserirConj(conjB,quantB);
+    printf("\nIMPRIMIR CONJUNTO A----------------------");
+    imprimir(conjA,quantA);
+    printf("\nIMPRIMIR CONJUNTO B----------------------");
+    imprimir(conjB,quantB);
+    
+    
+
+    return 0;
+} 
 
 
 
